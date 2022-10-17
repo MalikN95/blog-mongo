99c948736311664368b64b2cc4aacaf0157b4b01
Malik <79022015+MalikN95@users.noreply.github.com>
2022-10-17 23:28:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -59,10 +59,6 @@ const PORT = process.env.PORT || 3000
 async function start(){
     try{
        
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
         
         app.listen(PORT, () => {
             console.log(`Server start on ${PORT} port`);
